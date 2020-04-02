9508e32cab55183ffa925f12da7cc5437f0f1fbb
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-02 23:23:41 +0200

    Extended Patient class for new functionality.

--- src/main/java/patients/Patient.java
+++ src/main/java/patients/Patient.java
@@ -10,4 +10,13 @@ public class Patient {
     private String name;
     private String surname;
     private String pesel;
+    private Double wallet;
+
+    public boolean isSolvent(double price) {
+        return wallet > price;
+    }
+
+    public void payForTest(double value) {
+        wallet = wallet - value;
+    }
 }
