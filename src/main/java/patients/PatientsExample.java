64a46852f9c80799f6f2fb00747ce93af65ae111
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-03 23:46:59 +0200

    Some changes for prototyping

--- src/main/java/patients/PatientsExample.java
+++ src/main/java/patients/PatientsExample.java
@@ -1,8 +1,5 @@
 package patients;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class PatientsExample {
 
     private static final String FILE_PATH = "src/main/resources/PatientsList.xlsx";
@@ -10,22 +7,9 @@ public class PatientsExample {
 
     public static void main(String[] args) {
 
-        List<Patient> patients = Arrays.asList(
-                new Patient("John", "Cena", "12312323"),
-                new Patient("Alec", "White", "23455679"),
-                new Patient("Pablo", "Coelo", "12398467")
-        );
-
-        Patient patient = new Patient("George", "Kapeloni", "13398467");
-
-        PatientsService patientsService = new PatientsService(FILE_PATH, SHEET_NAME);
-
-        PatientsWriter.creatingFile(patients, FILE_PATH, SHEET_NAME);
-        PatientsWriter.creatingFile(patients, FILE_PATH, SHEET_NAME);
-
-        Patient foundPatient = patientsService.findPatient("12398467");
-        System.out.println(patientsService.patientExist("12398467"));
-        System.out.println(foundPatient);
-        PatientsWriter.addRecord(patient, FILE_PATH, SHEET_NAME);
+        String num = "12312.";
+        System.out.println(num.matches("([0-9]*)([.,][0-9]{2})?"));
+        String stringNumber = String.valueOf(23.3);
+        System.out.println(String.format("%.2s", stringNumber));
     }
 }
