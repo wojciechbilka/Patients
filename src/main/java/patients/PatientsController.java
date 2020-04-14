8a2e4a7b77da033e0106902a2df6876712c0564e
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-14 13:36:15 +0200

    Changed GrindPane for TableView (much more accurate for data presentation) Added method to update data Added method to perform test

--- src/main/java/patients/PatientsController.java
+++ src/main/java/patients/PatientsController.java
@@ -1,14 +1,8 @@
 package patients;
 
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-
+import javafx.scene.control.*;
 import java.io.File;
 import java.util.List;
 
@@ -18,18 +12,6 @@ public class PatientsController {
     private static final String SHEET_NAME = "patients";
 
     @FXML
-    GridPane grid;
-    @FXML
-    private Label gridLabel0;
-    @FXML
-    private Label gridLabel1;
-    @FXML
-    private Label gridLabel2;
-    @FXML
-    private Label gridLabel3;
-    @FXML
-    private Label gridLabel4;
-    @FXML
     private TextField nameField;
     @FXML
     private TextField surnameField;
@@ -38,139 +20,119 @@ public class PatientsController {
     @FXML
     private TextField balanceField;
     @FXML
+    private TextField testCostField;
+    @FXML
     private Label logMessage;
+    @FXML
+    private TableView<Patient> tableView;
+    @FXML
+    private ChoiceBox<TestResult> testResultChoiceBox;
 
-    PatientsService patientsService;
-    UserLog userLog;
-    PatientsValidator patientsValidator;
-
-    private EventHandler<MouseEvent> handleDoubleClick = event -> {
-        if (event.getClickCount() == 2) {
-            if (event.getSource() instanceof Label) {
-                Label label = (Label) event.getSource();
-                if (label.getParent() instanceof GridPane) {
-                    GridPane gp = (GridPane) label.getParent();
-                    for (Node node : gp.getChildren()) {
-                        if (node instanceof Label && GridPane.getRowIndex(node).equals(GridPane.getRowIndex(label))) {
-                            Label l = (Label) node;
-                            switch (GridPane.getColumnIndex(l)) {
-                                case 1:
-                                    nameField.setText(l.getText());
-                                    break;
-                                case 2:
-                                    surnameField.setText(l.getText());
-                                    break;
-                                case 3:
-                                    personalNumberField.setText(l.getText());
-                                    break;
-                                case 4:
-                                    balanceField.setText(l.getText());
-                                    break;
-                            }
-
-                        }
-                    }
-                }
-            }
-        }
-    };
+    private PatientsService patientsService;
+    private UserLog userLog;
+    private PatientsValidator patientsValidator;
+    private ObservableList<Patient> patients;
 
     @FXML
     private void initialize() {
         patientsService = new PatientsService(FILE_PATH, SHEET_NAME);
         userLog = new UserLog(logMessage);
-        patientsValidator = new PatientsValidator(patientsService, userLog);
+        patientsValidator = new PatientsValidator(userLog);
+        setRowDoubleClickHandler();
         initializeFile();
-        updateDisplay();
+        initializeTableView(patientsService.getPatients());
+        patients = tableView.getItems();
     }
 
     @FXML
     public void addPatient() {
-        if (allValid() & patientsValidator.validPatientNotExist(personalNumberField.getText())) {
-            Patient patient = new Patient(nameField.getText(), surnameField.getText(), personalNumberField.getText(), getTextFieldDoubleValue(balanceField));
-            patientsService.register(patient);
-            updateDisplay();
-            clearTextFields();
-            userLog.clearMessage();
+        if (allValid()) {
+            Patient patient = generateNewPatientFromFields();
+            if (patientsValidator.validatePatientNotExist(patients, patient)) {
+                patientsService.register(patient);
+                patients.add(patient);
+                clearTextFields();
+                userLog.clearMessage();
+            }
         }
     }
 
     @FXML
     public void removePatient() {
-        if (allValid() & patientsValidator.validatePatientExist(personalNumberField.getText())) {
-            Patient patient = new Patient(nameField.getText(), surnameField.getText(), personalNumberField.getText(), getTextFieldDoubleValue(balanceField));
-            patientsService.removePatient(patient);
-            updateDisplay();
-            clearTextFields();
-            userLog.clearMessage();
+        if (allValid()) {
+            Patient patient = getPatientAccordingToFields();
+            if (patientsValidator.validatePatientExist(patients, patient)) {
+                patientsService.removePatient(patient);
+                patients.remove(patient);
+                clearTextFields();
+                userLog.clearMessage();
+            }
         }
     }
 
     @FXML
-    private void performTest() {
-    }
-
-    private void markPatientRow(Patient patient, TestResult testResult) {
-        ObservableList<Node> children = grid.getChildren();
-        int rowIndex = -1;
-        for (Node node : children) {
-            if (node instanceof Label && node.getAccessibleText().equals(patient.getPesel())) {
-                rowIndex = GridPane.getRowIndex(node);
-            }
-        }
-        if (rowIndex != -1) {
-            for (Node node : children) {
-                if ((node instanceof Label) && GridPane.getRowIndex(node) == rowIndex) {
-                    Label temp = (Label) node;
-                    temp.styleProperty().setValue("error");
-                    break;
+    public void updatePatientData() {
+        if (allValid()) {
+            Patient patient = tableView.getSelectionModel().getSelectedItem();
+            if (patientsValidator.validatePatientExist(patients, patient)) {
+                Patient updatedPatient = generateNewPatientFromFields();
+                boolean personalNumberEquals = patient.getPersonalNumber().equals(updatedPatient.getPersonalNumber());
+                if (personalNumberEquals || patientsValidator.validatePatientsPersonalNumberIsUnique(patients, updatedPatient.getPersonalNumber())) {
+                    int selectionIndex = tableView.getSelectionModel().getSelectedIndex();
+                    patients.set(patients.indexOf(patient), updatedPatient);
+                    patientsService.updatePatientData(patient, updatedPatient);
+                    tableView.getSelectionModel().select(selectionIndex);
                 }
             }
         }
     }
 
-    private void updateDisplay() {
-        List<Patient> patients = patientsService.getPatients();
-        System.out.println(patients);
-        int colNum = grid.getColumnConstraints().size();
-        int rowNum = grid.getRowConstraints().size();
-        grid.getChildren().remove(5, grid.getChildren().size());
-        for (int r = 1; r < rowNum; r++) {
-            for (int c = 0; c <= colNum; c++) {
-                removeNodeByRowColumnIndex(r, c, grid);
-                System.out.println("row: " + r + "column: " + c);
+    @FXML
+    private void performTest() {
+        if (allValid()) {
+            Patient patient = getPatientAccordingToFields();
+            if (patientsValidator.validateTextFieldIsAnIntegerOr2DecimalNumber(testCostField) & patientsValidator.validatePatientExist(patients, patient)) {
+                Patient patientClone = patient.clone();
+                double cost = getTextFieldDoubleValue(testCostField);
+                if (patientsValidator.validatePatientIsSolvent(patient, cost)) {
+                    patient.performTest(cost);
+                    patientsService.updatePatientData(patientClone, patient);
+                    clearTextFields();
+                    userLog.clearMessage();
+                }
             }
-        }
-        int i = 1;
-        for (Patient patient : patients) {
-            Label tempName = new Label(patient.getName());
-            Label tempSurname = new Label(patient.getSurname());
-            Label tempPersonalNumber = new Label(patient.getPesel());
-            Label tempWallet = new Label(String.format("%.2f", patient.getWallet()).replace(',', '.'));
-
-            tempName.setOnMouseClicked(handleDoubleClick);
-            tempSurname.setOnMouseClicked(handleDoubleClick);
-            tempPersonalNumber.setOnMouseClicked(handleDoubleClick);
-            tempWallet.setOnMouseClicked(handleDoubleClick);
-
-            grid.addRow(i, new Label(String.valueOf(i)));
-            grid.addRow(i, tempName);
-            grid.addRow(i, tempSurname);
-            grid.addRow(i, tempPersonalNumber);
-            grid.addRow(i, tempWallet);
-            i++;
         }
     }
 
-    public void removeNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
-        ObservableList<Node> children = gridPane.getChildren();
-        for (Node node : children) {
-            if (node instanceof Label && GridPane.getRowIndex(node).equals(row) && GridPane.getColumnIndex(node).equals(column)) {
-                Label label = (Label) node;
-                gridPane.getChildren().remove(label);
-                break;
+    @FXML
+    private void clearTextFields() {
+        nameField.clear();
+        surnameField.clear();
+        personalNumberField.clear();
+        balanceField.clear();
+        testResultChoiceBox.getSelectionModel().clearSelection();
+    }
+
+    private Patient generateNewPatientFromFields() {
+        TestResult testResult = testResultChoiceBox.getSelectionModel().getSelectedItem();
+        if (testResult == null) {
+            testResult = TestResult.NOT_PERFORMED;
+        }
+        return new Patient(nameField.getText(), surnameField.getText(), personalNumberField.getText(), getTextFieldDoubleValue(balanceField), testResult);
+    }
+
+    private Patient getPatientAccordingToFields() {
+        for (Patient p : patients) {
+            if (nameField.getText().equals(p.getName()) &&
+                    surnameField.getText().equals(p.getSurname()) &&
+                    personalNumberField.getText().equals(p.getPersonalNumber()) &&
+                    testResultChoiceBox.getSelectionModel().getSelectedItem().equals(p.getTestResult()) &&
+                    balanceField.getText().equals(String.format("%.2f", p.getBalance()).replace(',', '.'))
+            ) {
+                return p;
             }
         }
+        return null;
     }
 
     private void initializeFile() {
@@ -180,39 +142,42 @@ public class PatientsController {
         }
     }
 
-    private void clearTextFields() {
-        nameField.clear();
-        surnameField.clear();
-        personalNumberField.clear();
-        balanceField.clear();
+    private void initializeTableView(List<Patient> patients) {
+        for (Patient p : patients) {
+            tableView.getItems().add(p);
+        }
     }
 
     private boolean allValid() {
         boolean nameFieldCorrect = patientsValidator.validateTextFieldConsistsOfLetters(nameField);
         boolean surnameFieldCorrect = patientsValidator.validateTextFieldConsistsOfLetters(surnameField);
         boolean personalNumberCorrect = patientsValidator.validateTextFieldIsAIntegerNumberOfXDigit(personalNumberField, 8);
-        boolean walletFieldCorrect = patientsValidator.validateTextFieldIsAnIntegerOr2DecimalNumber(balanceField);
-        return nameFieldCorrect && surnameFieldCorrect && personalNumberCorrect && walletFieldCorrect;
+        boolean balanceFieldCorrect = patientsValidator.validateTextFieldIsAnIntegerOr2DecimalNumber(balanceField);
+        return nameFieldCorrect && surnameFieldCorrect && personalNumberCorrect && balanceFieldCorrect;
     }
 
-    // add filter for only number access
     private Double getTextFieldDoubleValue(TextField textField) {
-        double doubleNumber = Double.parseDouble(textField.getText().isEmpty() ?
-                "0" :
-                textField.getText().replace(',', '.'));
-        return doubleNumber;
+        return Double.parseDouble(
+                textField.getText().isEmpty() ?
+                        "0" :
+                        textField.getText().replace(',', '.')
+        );
     }
 
-    public Node getNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
-        Node result = null;
-        ObservableList<Node> children = gridPane.getChildren();
-
-        for (Node node : children) {
-            if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
-                result = node;
-                break;
-            }
-        }
-        return result;
+    private void setRowDoubleClickHandler() {
+        tableView.setRowFactory(tv -> {
+            TableRow<Patient> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (!row.isEmpty())) {
+                    Patient patient = row.getItem();
+                    nameField.setText(patient.getName());
+                    surnameField.setText(patient.getSurname());
+                    personalNumberField.setText(patient.getPersonalNumber());
+                    testResultChoiceBox.getSelectionModel().select(patient.getTestResult());
+                    balanceField.setText(String.format("%.2f", patient.getBalance()).replace(',', '.'));
+                }
+            });
+            return row;
+        });
     }
 }
