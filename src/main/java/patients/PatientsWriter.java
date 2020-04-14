11e4f774a6257a392bbd2ad028620cb688d15ccb
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-14 13:33:19 +0200

    Added method for updating row, and new column for test status

--- src/main/java/patients/PatientsWriter.java
+++ src/main/java/patients/PatientsWriter.java
@@ -21,6 +21,7 @@ public class PatientsWriter {
         row.createCell(1).setCellValue("Surname");
         row.createCell(2).setCellValue("Personal Number");
         row.createCell(3).setCellValue("Wallet");
+        row.createCell(4).setCellValue("TestStatus");
 
         try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
             workbook.write(fileOutputStream);
@@ -46,8 +47,9 @@ public class PatientsWriter {
             Row row = sheet.createRow(sheet.getLastRowNum() + 1);
             row.createCell(0).setCellValue(patient.getName());
             row.createCell(1).setCellValue(patient.getSurname());
-            row.createCell(2).setCellValue(patient.getPesel());
-            row.createCell(3).setCellValue(patient.getWallet().toString());
+            row.createCell(2).setCellValue(patient.getPersonalNumber());
+            row.createCell(3).setCellValue(patient.getBalance().toString());
+            row.createCell(4).setCellValue(patient.getTestResult().name());
             flag = true;
         }
 
@@ -57,6 +59,53 @@ public class PatientsWriter {
             e.printStackTrace();
         }
         return flag;
+    }
+
+    public static boolean updateRecord(Patient patientToUpdate, Patient updatedPatient, String filePath, String sheetName) {
+        XSSFWorkbook workbook;
+        XSSFSheet sheet;
+        boolean flag = false;
+
+        try {
+            workbook = new XSSFWorkbook(new FileInputStream(filePath));
+            sheet = workbook.getSheet(sheetName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return flag;
+        }
+
+        Row row;
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            row = sheet.getRow(i);
+            if (rowCorrespondsToPatient(row, patientToUpdate)) {
+                row.getCell(0).setCellValue(updatedPatient.getName());
+                row.getCell(1).setCellValue(updatedPatient.getSurname());
+                row.getCell(2).setCellValue(updatedPatient.getPersonalNumber());
+                row.getCell(3).setCellValue(updatedPatient.getBalance().toString());
+                row.getCell(4).setCellValue(updatedPatient.getTestResult().name());
+                flag = true;
+            }
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+            workbook.write(fileOutputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    private static boolean rowCorrespondsToPatient(Row row, Patient patient) {
+        if (    row.getCell(0).toString().equals(patient.getName()) &&
+                row.getCell(1).toString().equals(patient.getSurname()) &&
+                row.getCell(2).toString().equals(patient.getPersonalNumber()) &&
+                Double.valueOf(row.getCell(3).toString()).equals(patient.getBalance()) &&
+                TestResult.valueOf(row.getCell(4).toString()).equals(patient.getTestResult())) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public static boolean removeRecord(Patient patient, String filePath, String sheetName) {
@@ -73,7 +122,7 @@ public class PatientsWriter {
 
         int rowNum = -1;
         for (Row row : sheet) {
-            if ((row.getCell(2).toString().equals(patient.getPesel()))) {
+            if ((row.getCell(2).toString().equals(patient.getPersonalNumber()))) {
                 rowNum = row.getRowNum();
             }
         }
