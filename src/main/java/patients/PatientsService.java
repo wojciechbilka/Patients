ac45ae45ebb2ed9e939d513e6001685a72f7ca56
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-14 13:20:20 +0200

    Added method for updating data of specific patient + Some quick fixes

--- src/main/java/patients/PatientsService.java
+++ src/main/java/patients/PatientsService.java
@@ -15,56 +15,59 @@ public class PatientsService {
 
     public boolean register(Patient patient) {
         boolean flag = false;
-        if (PatientsReader.isUnique(patient, path, sheetName) && !patient.getPesel().isEmpty()) {
+        if (PatientsReader.isUnique(patient, path, sheetName) && !patient.getPersonalNumber().isEmpty()) {
             PatientsWriter.addRecord(patient, path, sheetName);
             flag = true;
         }
         return flag;
     }
 
-    public Patient findPatient(String pesel) {
+    public Patient findPatient(String personalNumber) {
         List<Patient> patients = PatientsReader.readFile(path, sheetName);
         for (Patient p : patients) {
-            if (p.getPesel().equals(pesel)) {
+            if (p.getPersonalNumber().equals(personalNumber)) {
                 return p;
             }
         }
-        System.out.println("There is no patient in list");
+        System.out.println("There is no patient on the list");
         return null;
     }
 
-    public Patient findPatient(String name, String surname) {
+    public Patient findPatient(Patient patient) {
         List<Patient> patients = PatientsReader.readFile(path, sheetName);
         for (Patient p : patients) {
-            if (p.getName().equals(name) || p.getSurname().equals(surname)) {
+            if (p.equals(patient)) {
                 return p;
             }
         }
-        System.out.println("There is no patient in list");
+        System.out.println("There is no patient on the list");
         return null;
     }
 
-    public boolean patientExist(String pesel) {
+    public boolean patientExist(String personalNumber) {
         List<Patient> patients = PatientsReader.readFile(path, sheetName);
         for (Patient p : patients) {
-            if (p.getPesel().equals(pesel)) {
+            if (p.getPersonalNumber().equals(personalNumber)) {
                 return true;
             }
         }
-        System.out.println("There is no such patient in list");
+        System.out.println("There is no such patient ont the list");
         return false;
     }
 
-    public boolean patientExist(String name, String surname) {
+    public boolean patientExist(Patient patient) {
         List<Patient> patients = PatientsReader.readFile(path, sheetName);
-
         for (Patient p : patients) {
-            if (p.getName().equals(name) || p.getSurname().equals(surname)) {
+            if (p.equals(patient)) {
                 return true;
             }
         }
-        System.out.println("There is no such patient in list");
+        System.out.println("There is no such patient on the list");
         return false;
+    }
+
+    public boolean updatePatientData(Patient patientBeforeUpdate, Patient patientWithUpdatedData) {
+        return PatientsWriter.updateRecord(patientBeforeUpdate, patientWithUpdatedData, path, sheetName);
     }
 
     public List<Patient> getPatients() {
